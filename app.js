d7a328d4de73273a9b9faa035fdd6f6b47321d91
Dan Meisel <dmeis654@gmail.com>
2017-06-13 11:59:06 -0400

    12 noon edits

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
 const express = require( 'express' );
+const nunjucks = require('nunjucks');
 const app = express();
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+const routes = require('./routes');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    console.log(output);
+});
+
+app.use('/', routes);
+
+app.use(express.static('public'));
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var list = tweetBank.find( {name: name} );
+  res.render( 'index', {list: list} );
+});
+
+// app.use(function (req, res, next) {
+//     console.log("hi")
+//     next()
+// })
+
+// app.get('/', function (req, res) {
+//   res.render( 'index', {title: 'Hall of Fame', people: locals.people} );
+// })
+
+
 
 app.listen(3000, function(){
     console.log("server listening")
